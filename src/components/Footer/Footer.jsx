2caeec6b49b6e48f50a1fcb6d0df9b62bf94531f
ignaciorodriguezbeis <igna173arg@gmail.com>
2026-07-17 19:30:59 -0300

    new file:   src/assets/img/usuario (1).png 	modified:   src/components/Footer/Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,17 +6,18 @@ function Footer() {
         <footer>
             <div class="footer-social">
                 <a href="https://www.facebook.com/" target="_blank">
-                    <img src="/src/assets/img/facebook-icon.png" alt="Facebook"></img>
+                    <img src="./assets/img/facebook-icon.png" alt="Facebook"></img>
                 </a>
                 <a href="https://www.instagram.com/" target="_blank">
-                    <img src="/src/assets/img/instagram-icon.png" alt="Instagram"></img>
+                    <img src="./assets/img/instagram-icon.png" alt="Instagram"></img>
                 </a>
                 <a href="https://web.whatsapp.com/" target="_blank">
-                    <img src="/src/assets/img/whatsapp-icon.png" alt="WhatsApp"></img>
+                    <img src="./assets/img/whatsapp-icon.png" alt="WhatsApp"></img>
                 </a>
             </div>
             <div>
-                <p>© 2025 Krakenshop - Tu tienda de juegos de mesa. derechos reservados.</p>
+                <p> Para más información, contáctanos a través de nuestras redes sociales o al +54 9 3541 123456. Estamos disponibles para responder tus consultas y brindarte asistencia en todo lo relacionado con el turismo en Villa Carlos Paz. </p>
+                    <p>© 2025 VCP turismo - Todos los derechos reservados.</p>
             </div>
 
         </footer >
