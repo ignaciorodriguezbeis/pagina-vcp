82b639474a32b4fb4df78219869956474d4bfa4e
ignaciorodriguezbeis <igna173arg@gmail.com>
2026-08-11 22:31:50 -0300

    modificando el footer

--- pagina VCP/src/components/Footer/Footer.jsx
+++ pagina VCP/src/components/Footer/Footer.jsx
@@ -1,45 +1,86 @@
 import React from "react";
 import "./Footer.css";
-import facebook from '../../assets/img/facebook-icon.png';
-import instagram from '../../assets/img/instagram-icon.png';
-import whatsapp from '../../assets/img/whatsapp-icon.png';
-import logo from '../../assets/img/cucu_logo.jpg'
+import facebook from "../../assets/img/facebook-icon.png";
+import instagram from "../../assets/img/instagram-icon.png";
+import whatsapp from "../../assets/img/whatsapp-icon.png";
+import logo from "../../assets/img/cucu_logo.jpg";
 
 function Footer() {
-    return (
-        <footer>
-
-            <div className="footerheader">
-
-                <div>
-                    <img src={logo} alt="" />
-                    <h1>VCP turismo</h1>
-                </div>
-
-                <p>Guía independiente de turismo de Villa Carlos Paz, Valle de Punilla, Córdoba, Argentina.</p>
-                
-            </div>
-
-            <div className="footer-social">
-                <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
-                    <img src={facebook} alt="Facebook" />
-                </a>
-                <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
-                    <img src={instagram} alt="Instagram" />
-                </a>
-                <a href="https://web.whatsapp.com/" target="_blank" rel="noreferrer">
-                    <img src={whatsapp} alt="WhatsApp" />
-                </a>
-            </div>
-
+  return (
+    <footer className="footer">
+      <div className="footer-top">
+        <div className="footer-brand">
+          <div className="brand-logo">
+            <img src={logo} alt="VCP turismo" />
             <div>
-                <p> Para más información, contáctanos a través de nuestras redes sociales o al +54 9 3541 123456. Estamos disponibles para responder tus consultas y brindarte asistencia en todo lo relacionado con el turismo en Villa Carlos Paz. </p>
-                <p>© 2025 VCP turismo - Todos los derechos reservados.</p>
+              <p className="brand-label">CarlosPaz.com</p>
             </div>
+          </div>
+          <p className="brand-description">
+            Guía independiente de turismo de Villa Carlos Paz, Valle de Punilla,
+            Córdoba, Argentina.
+          </p>
+        </div>
 
-        </footer >
-    )
+        <div className="footer-columns">
+          <div className="footer-column">
+            <h3>Alojamiento</h3>
+            <ul>
+              <li>Hoteles</li>
+              <li>Cabañas y aparts</li>
+              <li>Hostels</li>
+              <li>Alquileres temporarios</li>
+              <li>Campings</li>
+            </ul>
+          </div>
+
+          <div className="footer-column">
+            <h3>Qué hacer</h3>
+            <ul>
+              <li>Excursiones</li>
+              <li>Espectáculos</li>
+              <li>Aventura</li>
+              <li>Con niños</li>
+            </ul>
+          </div>
+
+          <div className="footer-column">
+            <h3>Comer</h3>
+            <ul>
+              <li>Parrillas</li>
+              <li>Cafés y confiterías</li>
+              <li>Cervecerías</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer-business">
+        <h3>Para negocios</h3>
+        <ul>
+          <li>Anunciá tu negocio</li>
+          <li>Publicar una oferta</li>
+        </ul>
+      </div>
+
+      <div className="footer-bottom">
+        <div className="footer-social">
+          <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+            <img src={facebook} alt="Facebook" />
+          </a>
+          <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+            <img src={instagram} alt="Instagram" />
+          </a>
+          <a href="https://web.whatsapp.com/" target="_blank" rel="noreferrer">
+            <img src={whatsapp} alt="WhatsApp" />
+          </a>
+        </div>
+
+        <p>© 2026 CarlosPaz.com — Sitio de demostración. Villa Carlos Paz, Córdoba.</p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
 
