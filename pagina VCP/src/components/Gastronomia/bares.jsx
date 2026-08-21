aa4c5da0bf247deacdd4eef925327a3e18948da1
ignaciorodriguezbeis <igna173arg@gmail.com>
2026-08-20 23:41:08 -0300

    corrigiendo links gastro

--- pagina VCP/src/components/Gastronomia/bares.jsx
+++ pagina VCP/src/components/Gastronomia/bares.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 import './bares.css';
 
 import Inicio from '../Inicio/Inicio';
@@ -63,7 +63,7 @@ function Bares() {
   return (
     <body className='bares'>
       <header style={{ backgroundImage: `url(${bar1})` }}>
-        <p><a href="/inicio">Inicio</a> &gt; <a href="gastronomia">Gastronomia</a> &gt; Bares </p>
+        <p><Link to="/inicio">Inicio</Link> &gt; <Link to="/actividades">Gastronomia</Link> &gt; Bares </p>
         <h1>Bares de Villa Carlos Paz</h1>
         <p>Bienvenido a la sección de gastronomía de VCP Turismo. Aquí encontrarás información sobre los mejores restaurantes, bares y lugares para disfrutar de la deliciosa comida local en Villa Carlos Paz. Explora nuestras recomendaciones y descubre los sabores únicos que esta ciudad tiene para ofrecer.</p>
       </header>
