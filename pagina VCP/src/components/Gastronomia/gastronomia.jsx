9f6a23c525d52184b9f2e88560a1358973d18f2d
ignaciorodriguezbeis <igna173arg@gmail.com>
2026-08-31 22:48:36 -0300

    arreglando enlaces

--- pagina VCP/src/components/Gastronomia/gastronomia.jsx
+++ pagina VCP/src/components/Gastronomia/gastronomia.jsx
@@ -55,13 +55,14 @@ function Gastronomia() {
                         <article>
                             <GastronomiaSlider>
                                 {restaurantes.map((restaurante, index) => (
-                                    <article key={`${restaurante.titulo}-${index}`}>
-                                        <img src={restaurante.imagen} alt={restaurante.titulo} />
-                                        <h3>{restaurante.titulo}</h3>
+                                    <article key={`${restaurante.nombre}-${index}`}>
+                                        <img src={restaurante.imagen} alt={restaurante.nombre} />
+                                        <h3>{restaurante.nombre}</h3>
                                         <p>{restaurante.descripcion}</p>
                                         <h4>dirección: {restaurante.direccion}</h4>
+                                        <p>horario:{restaurante.horario}</p>
                                         <p>teléfono: {restaurante.telefono}</p>
-                                        <h5>precio: {restaurante.precio}</h5>
+                                        <h5>delivery: {restaurante.delivery}</h5>
                                     </article>
                                 ))}
                             </GastronomiaSlider>
@@ -73,13 +74,14 @@ function Gastronomia() {
                         <article>
                             <GastronomiaSlider>
                                 {bares.map((bar, index) => (
-                                    <article key={`${bar.titulo}-${index}`}>
-                                        <img src={bar.imagen} alt={bar.titulo} />
-                                        <h3>{bar.titulo}</h3>
+                                    <article key={`${bar.nombre}-${index}`}>
+                                        <img src={bar.imagen} alt={bar.nombre} />
+                                        <h3>{bar.nombre}</h3>
                                         <p>{bar.descripcion}</p>
                                         <h4>dirección: {bar.direccion}</h4>
+                                        <p>horario:{bar.horario}</p>
                                         <p>teléfono: {bar.telefono}</p>
-                                        <h5>precio: {bar.precio}</h5>
+                                        <h5>delivery: {bar.delivery}</h5>
                                     </article>
                                 ))}
                             </GastronomiaSlider>
@@ -91,13 +93,14 @@ function Gastronomia() {
                         <article>
                             <GastronomiaSlider>
                                 {heladerias.map((heladeria, index) => (
-                                    <article key={`${heladeria.titulo}-${index}`}>
-                                        <img src={heladeria.imagen} alt={heladeria.titulo} />
-                                        <h3>{heladeria.titulo}</h3>
+                                    <article key={`${heladeria.nombre}-${index}`}>
+                                        <img src={heladeria.imagen} alt={heladeria.nombre} />
+                                        <h3>{heladeria.nombre}</h3>
                                         <p>{heladeria.descripcion}</p>
                                         <h4>dirección: {heladeria.direccion}</h4>
+                                        <p>horario:{heladeria.horario}</p>
                                         <p>teléfono: {heladeria.telefono}</p>
-                                        <h5>precio: {heladeria.precio}</h5>
+                                        <h5>delivery: {heladeria.delivery}</h5>
                                     </article>
                                 ))}
                             </GastronomiaSlider>
@@ -109,13 +112,14 @@ function Gastronomia() {
                         <article>
                             <GastronomiaSlider>
                                 {cafeterias.map((cafeteria, index) => (
-                                    <article key={`${cafeteria.titulo}-${index}`}>
-                                        <img src={cafeteria.imagen} alt={cafeteria.titulo} />
-                                        <h3>{cafeteria.titulo}</h3>
+                                    <article key={`${cafeteria.nombre}-${index}`}>
+                                        <img src={cafeteria.imagen} alt={cafeteria.nombre} />
+                                        <h3>{cafeteria.nombre}</h3>
                                         <p>{cafeteria.descripcion}</p>
                                         <h4>dirección: {cafeteria.direccion}</h4>
+                                        <p>horario:{cafeteria.horario}</p>
                                         <p>teléfono: {cafeteria.telefono}</p>
-                                        <h5>precio: {cafeteria.precio}</h5>
+                                        <h5>delivery: {cafeteria.delivery}</h5>
                                     </article>
                                 ))}
                             </GastronomiaSlider>
@@ -127,13 +131,13 @@ function Gastronomia() {
                         <article>
                             <GastronomiaSlider>
                                 {cervecerias.map((cerveceria, index) => (
-                                    <article key={`${cerveceria.titulo}-${index}`}>
-                                        <img src={cerveceria.imagen} alt={cerveceria.titulo} />
-                                        <h3>{cerveceria.titulo}</h3>
+                                    <article key={`${cerveceria.nombre}-${index}`}>
+                                        <img src={cerveceria.imagen} alt={cerveceria.nombre} />
+                                        <h3>{cerveceria.nombre}</h3>
                                         <p>{cerveceria.descripcion}</p>
                                         <h4>dirección: {cerveceria.direccion}</h4>
                                         <p>teléfono: {cerveceria.telefono}</p>
-                                        <h5>precio: {cerveceria.precio}</h5>
+                                        <h5>delivery: {cerveceria.delivery}</h5>
                                     </article>
                                 ))}
                             </GastronomiaSlider>
