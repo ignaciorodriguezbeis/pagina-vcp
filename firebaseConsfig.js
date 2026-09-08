84976b693fe40975f19b1125ad9d0f9eb65c880b
ignaciorodriguezbeis <igna173arg@gmail.com>
2026-09-08 12:00:28 -0300

    agregando firebase

---
+++ firebaseConsfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwmTFADvpKUurEKE_ShOkj1viHzIcwmNo",
+  authDomain: "pagina-vcp-ef6af.firebaseapp.com",
+  projectId: "pagina-vcp-ef6af",
+  storageBucket: "pagina-vcp-ef6af.firebasestorage.app",
+  messagingSenderId: "76012429321",
+  appId: "1:76012429321:web:af01176fd9e5f1518118d8",
+  measurementId: "G-CMY44CZ847"
+};
+
+
+export const firebaseApp = initializeApp(firebaseConfig);
